7c0f0246ca6017692c619b1918b2527cec979d3a
Jonathan Kupcho <jonathankupcho@northwesternmutual.com>
2019-02-27 12:49:42 -0600

    put schema in its own file

---
+++ api/src/schema.js
@@ -0,0 +1,103 @@
+const { gql } = require("apollo-server-express");
+
+// Construct a schema, using GraphQL schema language
+exports.typeDefs = gql`
+  scalar DateTime
+
+  enum PaymentType {
+    CREDIT
+    DEBIT
+    PAYPAL
+  }
+
+  interface Payment {
+    id: Int!
+    total: Float!
+    paymentType: PaymentType!
+  }
+
+  type Credit implements Payment {
+    id: Int!
+    total: Float!
+    paymentType: PaymentType!
+    authorizationCode: String!
+  }
+
+  type Debit implements Payment {
+    id: Int!
+    total: Float!
+    paymentType: PaymentType!
+    bankAuthCode: String!
+  }
+
+  type PayPal implements Payment {
+    id: Int!
+    total: Float!
+    paymentType: PaymentType!
+    chargeBack: Boolean!
+  }
+
+  union OrderPayment = Credit | Debit | PayPal
+
+  input PageInput {
+    limit: Int!
+    page: Int!
+  }
+
+  type Customer {
+    id: Int
+    firstName: String
+    lastName: String
+    email: String
+    phone: String
+    address: Address
+    orders: [Order]
+  }
+
+  type Address {
+    streetAddress: String
+    city: String
+    state: String
+    zipCode: String
+  }
+
+  type Order {
+    id: Int
+    customer: Customer
+    paymentType: String
+    ordered: DateTime
+    shipped: DateTime
+    lineItems: [LineItem]
+  }
+
+  type CustomerPage {
+    customers: [Customer]
+    pageInfo: PageInfo
+  }
+
+  type PageInfo {
+    limit: Int!
+    page: Int!
+    hasNext: Boolean!
+    numPages: Int!
+  }
+
+  type LineItem {
+    product: Product
+    quantity: Int
+  }
+
+  type Product {
+    name: String
+    company: String
+    retailPrice: Float
+    sku: String
+  }
+
+  type Query {
+    customers(pageInput: PageInput!): CustomerPage
+    customerOrders(customerId: Int!): [Order]
+    customer(id: Int!): Customer
+    orderPayments: [OrderPayment]
+  }
+`;
